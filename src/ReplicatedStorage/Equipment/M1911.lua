8854396ff1e5199a1a1b4b17d05ba2297c519986
poopnugget142 <68136791+poopnugget142@users.noreply.github.com>
2023-08-29 22:04:46 -0700

    Work on gun animations and model

--- src/ReplicatedStorage/Equipment/M1911.lua
+++ src/ReplicatedStorage/Equipment/M1911.lua
@@ -12,9 +12,12 @@
 --initialise dependencies
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
 
 local Remotes = ReplicatedStorage.Remotes
+local Assets = ReplicatedStorage.Assets
 
+local EquipmentStates = require(ReplicatedStorage.Scripts.States.Equipment)
 local KeyBindings = require(ReplicatedStorage.Scripts.Util.KeyBindings)
 local GunModule = require(ReplicatedStorage.Scripts.Class.Gun)
 local Enums = require(ReplicatedStorage.Scripts.Enums)
@@ -28,29 +31,107 @@ local Player = Players.LocalPlayer
 
 local Module = {}
 
-Module.Give = function(Entity, ItemModel)
+Module.Give = function(Entity)
+    local Model = Assets.Guns.M1911:Clone()
+    EquipmentStates.LoadingItem.add(Entity, Model)
 
+    local Handle = Model.Handle
+    local Grip = Handle.Grip
+
+    local Character = Player.Character
+
+    local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+
+    Model.Parent = Character
+    Grip.Part1 = Character.RightHand
+
+    --IK WILL MOVE SOMEWEHRE ELSE LATER TRUST ME THIS IS TOO LONG
+    --IK Attachments
+    local IKGoal = Instance.new("Attachment")
+    IKGoal.Parent = HumanoidRootPart
+    IKGoal.Position = Vector3.new(1.5, 0.5, -1.5)
+    IKGoal.CFrame *= CFrame.Angles(math.rad(90), 0, 0)
+    IKGoal.Name = "IKGoal"
+
+    local Pole = Instance.new("Attachment")
+    Pole.Parent = HumanoidRootPart
+    Pole.Position = Vector3.new(1, 0, 1)
+    Pole.Name = "Pole"
+
+    --Elbow Constraint
+    local RightElbowConstraint = Instance.new("HingeConstraint")
+    RightElbowConstraint.Visible = true
+    RightElbowConstraint.Parent = Character.RightLowerArm
+    RightElbowConstraint.Name = "RightElbowConstraint"
+
+    local RightElbowConstraintAttachment0 = Instance.new("Attachment")
+    RightElbowConstraintAttachment0.Parent = Character.RightUpperArm.RightElbowRigAttachment
+
+    local RightElbowConstraintAttachment1 = Instance.new("Attachment")
+    RightElbowConstraintAttachment1.Parent = Character.RightLowerArm.RightElbowRigAttachment
+
+    RightElbowConstraintAttachment1.CFrame = RightElbowConstraintAttachment0.CFrame
+
+    RightElbowConstraint.Attachment0 = RightElbowConstraintAttachment0
+    RightElbowConstraint.Attachment1 = RightElbowConstraintAttachment1
+
+    --Wrist Constraint
+    local RightWristConstraint = Instance.new("BallSocketConstraint")
+    RightWristConstraint.Parent = Character.RightHand
+    RightWristConstraint.Name = "LeftWristConstraint"
+
+    local RightWristConstraintAttachment0 = Instance.new("Attachment")
+    RightWristConstraintAttachment0.CFrame *= CFrame.Angles(0, math.rad(-180), math.rad(-90))
+    RightWristConstraintAttachment0.Parent = Character.RightLowerArm.RightWristRigAttachment
+
+    local RightWristConstraintAttachment1 = Instance.new("Attachment")
+    RightWristConstraintAttachment1.Parent = Character.RightHand.RightWristRigAttachment
+
+    RightWristConstraintAttachment1.CFrame = RightWristConstraintAttachment0.CFrame
+
+    RightWristConstraint.Attachment0 = RightWristConstraintAttachment0
+    RightWristConstraint.Attachment1 = RightWristConstraintAttachment1
+
+    RightWristConstraint.LimitsEnabled = true
+    RightWristConstraint.UpperAngle = 80
+
+    --IK Control Set up
+    local IKControl = Instance.new("IKControl")
+    IKControl.Name = "RightArmControl"
+    IKControl.SmoothTime = 0.1
+    IKControl.Pole = Pole
+
+    IKControl.ChainRoot = Character.RightUpperArm
+    IKControl.EndEffector = Character.RightHand
+
+    IKControl.Type = Enum.IKControlType.Transform
+    IKControl.Target = IKGoal
+    IKControl.Parent = Character.Humanoid
 end
 
 Module.ServerGotItemID = function(Entity, ItemID)
     local shooting = false
 
+    local EquipmentData = EquipmentStates.World.get(Entity)
+
+    local Model = EquipmentData.Model
 
     KeyBindings.BindAction("Attack", Enum.UserInputState.Begin, function()
         shooting = true
         local deviation = Vector3.zero
 
         while shooting do
-            --print(shooting)
             task.wait(60/600)
             local Character = Player.Character
             --local HumanoidRootPart = Character.Model.PrimaryPart
             local HumanoidRootPart = Character.PrimaryPart
-            local Origin = HumanoidRootPart.Position
+            local Origin = Model.Muzzle.Position
 
             local BulletResult = GunModule.BulletShoot(Origin)
 
             --AlphaPart.Spawn(CastBehaviour, Origin, TerrainResult.Position * Vector3.new(1,0,1) + Origin * Vector3.yAxis, 200)
+
+            if not BulletResult.TerrainResult then continue end
 
             GunModule.CreateTracer(Origin, BulletResult.TerrainResult.Position, GunEnum, Enums.Bullet["9mmTracer"])
 
@@ -60,7 +141,6 @@ Module.ServerGotItemID = function(Entity, ItemID)
 
     KeyBindings.BindAction("Attack", Enum.UserInputState.End, function()
         shooting = false
-        --print(shooting)
     end)
 end
 

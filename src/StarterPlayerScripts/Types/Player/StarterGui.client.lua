e6a5f4c16e4ba8aa9bf3fda546c2cdc705ee66c7
poopnugget142 <68136791+poopnugget142@users.noreply.github.com>
2023-09-01 20:51:09 -0700

    readded chat

--- src/StarterPlayerScripts/Types/Player/StarterGui.client.lua
+++ src/StarterPlayerScripts/Types/Player/StarterGui.client.lua
@@ -1,4 +1,4 @@
 local StarterGui = game:GetService("StarterGui")
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
-StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
+--StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)

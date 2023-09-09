563ccd38f95273c524ca8bfcb87d6b2d6dbc3e9e
poopnugget142 <68136791+poopnugget142@users.noreply.github.com>
2023-09-09 01:01:34 -0700

    data

--- src/ReplicatedStorage/Registry/NPC.lua
+++ src/ReplicatedStorage/Registry/NPC.lua
@@ -17,4 +17,25 @@ Module.GetBaddieModel = function(BaddieEnum : number)
     return BaddieModels[BaddieEnum]
 end
 
+--Defines the distance for a npc to be considered close to another one and move away
+local NearbyNpcDistance = {
+    [Enums.NPC.Guy] = 2;
+}
+
+Module.GetNearbyNpcDistance = function(BaddieEnum : number)
+    assert(NearbyNpcDistance[BaddieEnum], "BaddieEnum "..BaddieEnum.." has no nearby npc distance")
+
+    return NearbyNpcDistance[BaddieEnum]
+end
+
+local AttackRange = {
+    [Enums.NPC.Guy] = 10;
+}
+
+Module.GetAttackRange = function(BaddieEnum : number)
+    assert(AttackRange[BaddieEnum], "BaddieEnum "..BaddieEnum.." has no attack range")
+
+    return AttackRange[BaddieEnum]
+end
+
 return Module
--- src/ServerScriptService/Character.server.lua
+++ src/ServerScriptService/Character.server.lua
@@ -5,11 +5,16 @@ local ServerStorage = game:GetService("ServerStorage")
 local Assets = ReplicatedStorage.Assets
 
 local Equipment = require(ServerStorage.Scripts.Equipment)
+local CharacterModule = require(ReplicatedStorage.Scripts.Class.Character)
+local CharacterStates = require(ReplicatedStorage.Scripts.States.Character)
 
 Players.PlayerAdded:Connect(function(Player)
     Equipment.AddEquipment(Player, "Shotgun")
     Player.CharacterAdded:Connect(function(Character)
         task.wait()
         Character.Parent = workspace.Characters.Players
+
+        local Entity = CharacterStates.World.entity()
+        CharacterModule.RegisterCharacter(Entity, Character)
     end)
 end)

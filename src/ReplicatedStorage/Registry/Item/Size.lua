915d14e933313db192e86a5c2f283e5d9549ed58
poopnugget142 <68136791+poopnugget142@users.noreply.github.com>
2023-12-23 17:35:08 -0800

    testing commit

--- src/ReplicatedStorage/Registry/Item/Size.lua
+++ src/ReplicatedStorage/Registry/Item/Size.lua
@@ -3,6 +3,7 @@ local ReplicatedScripts = ReplicatedStorage:WaitForChild("Scripts")
 local Enums = require(ReplicatedScripts.Registry.Enums)
 local Item = Enums.Item
 
+--Defines the size of the item in the inventory
 return {
     [Item.Shotgun] = {4, 2}
 }
